810c4d788e84e7f7245ca72280c509ae3bd50b75
stuffundefined <sforman157@gmail.com>
2023-06-07 10:30:39 -0400

    remove debug print statements

--- dragging.js
+++ dragging.js
@@ -1,13 +1,11 @@
 let center = document.getElementById('center')
 center.addEventListener('pointerdown', ev => {
-    // console.log(ev.target)
     if (ev.target == center) {
         center.style.top = (ev.y - ev.offsetY + 150) + 'px';
         center.style.left = (ev.x - ev.offsetX + 150) + 'px';
     }
 })
 center.addEventListener('pointermove', ev => {
-    console.log(ev.target.tagName)
     if (ev.buttons && center.style.top && ev.target.tagName != 'A') {
         center.style.top = Number(center.style.top.substring(0, center.style.top.length - 2)) + ev.movementY + 'px';
         center.style.left = Number(center.style.left.substring(0, center.style.left.length - 2)) + ev.movementX + 'px';
