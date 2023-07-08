a62d7d793f0fc7222e1451338e07d63c3e2767c1
stuffundefined <sforman157@gmail.com>
2023-07-08 18:14:56 -0400

    possibly progress

--- dragging.js
+++ dragging.js
@@ -1,13 +1,15 @@
-let center = document.getElementById('center')
-center.addEventListener('pointerdown', ev => {
-    if (ev.target == center) {
-        center.style.top = (ev.y - ev.offsetY + 150) + 'px';
-        center.style.left = (ev.x - ev.offsetX + 150) + 'px';
-    }
-})
-center.addEventListener('pointermove', ev => {
-    if (ev.buttons && center.style.top && ev.target.tagName != 'A') {
-        center.style.top = Number(center.style.top.substring(0, center.style.top.length - 2)) + ev.movementY + 'px';
-        center.style.left = Number(center.style.left.substring(0, center.style.left.length - 2)) + ev.movementX + 'px';
-    }
-})
+let draggables = document.getElementsByClassName('movable')
+for (element of draggables) {
+    element.addEventListener('pointerdown', ev => {
+        if (ev.target == element) {
+            element.style.top = (ev.y - ev.offsetY + 150) + 'px';
+            element.style.left = (ev.x - ev.offsetX + 150) + 'px';
+        }
+    })
+    element.addEventListener('pointermove', ev => {
+        if (ev.buttons && element.style.top && ev.target.tagName != 'A') {
+            element.style.top = Number(element.style.top.substring(0, element.style.top.length - 2)) + ev.movementY + 'px';
+            element.style.left = Number(element.style.left.substring(0, element.style.left.length - 2)) + ev.movementX + 'px';
+        }
+    })
+}
